da4a8de9af7c865547dacf7fc87ff553b7d62bdc
kostya-alch <kostafrompiter@gmail.com>
2021-10-04 23:52:40 +0300

    add request for Back and request a list of lists

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -26,9 +26,9 @@ const Tasks = () => {
                         <path
                            d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001"
                            stroke="#000"
-                           stroke-width="1.5"
-                           stroke-linecap="round"
-                           stroke-linejoin="round"
+                           strokWidth="1.5"
+                           strokeLinecap="round"
+                           strokeLinejoin="round"
                         />
                      </svg>
                   </label>
